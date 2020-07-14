d2b41d8e1aef85aca3d0486e6d85261110de1270
Eddie Cheng <local@example.test>
2020-07-14 17:20:31 +0200

    added comment

--- callbacks.js
+++ callbacks.js
@@ -1,3 +1,4 @@
+// just lists
 const posts = [
   {title: 'Post one', body: 'This is post one'},
   {title: 'Post two', body: 'This is post two'}
@@ -17,9 +18,12 @@ const getPosts = () => {
 const createPosts = (post, callback) => {
   setTimeout(() => {
     posts.push(post);
-    // the function will be called after posts.push(post);
+    // The function will be called after posts.push(post);
+    // So here is getPosts called afterwards!!
+    // So the program will wait for 3 sec than wait for 1 sec
     callback();
   }, 3000);
 }
 
+// run whole prgram
 createPosts({title: 'Post three', body: 'This is post three'}, getPosts);
